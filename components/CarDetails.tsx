1a8063cf52e2ebe50587849b45b6e39fb94b9147
Stephane Noel <steftraveler33@gmail.com>
2023-07-31 16:48:08 +0200

    correction de textes/balises inutiles

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -74,7 +74,7 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
                       </div>
                       <div className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg">
                         <Image
-                          src={generateCarImagesUrl(car, "33")}
+                          src={generateCarImagesUrl(car, "22")}
                           alt="car model"
                           fill
                           priority
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -10,12 +10,11 @@ const Hero = () => {
     return (
         <div className="hero">
             <div className="flex-1 pt-36 padding-x">
-                <h1 className="hero__title">Find, book or rent a car --- quickly and easily</h1>title
+                <h1 className="hero__title">Find, book or rent a car --- quickly and easily</h1>
                 <p className="hero__subtitle">
                     Streamline your car rental experience with our effortless booking process.
                 </p>
                 <CustomButton title="Exlore Cars" containerStyles="bg-primary-blue text-white rounded-full mt-10" handleClick={handleScroll} />
-                custom-btn bg-primary-blue text-white rounded-full mt-10
             </div>
             <div className="hero__image-container">
                 <div className="hero__image">
